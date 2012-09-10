b27adace8c05fbd213a54c5f9732a167882d3d6f
Max Ludwig <maxe.ludwig@googlemail.com>
2012-09-11 01:07:02 +0200

    made it really damn simple

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-# fucking-simple-blogging
+# simple-wiki
+
+Okay, this is so damn simple. Everything depends on the title of a post (or
+page or whatever you want to call it). I use `urlsafe_b64(en|de)code` and
+Werkzeug's `url_(un)quote_plus` to convert a title into a valid filename and
+URL ... and back! That means, I totally don't care what crap the title contains
+because I always get a URL and a file name, no matter what. And that's the
+whole trick. Unbelievable, isn't it?
 
 ## Usage
 
 It's a Flask project. Look at the code and figure it out by yourself. Here are the required packages:
 
-    $ pip freeze -l
     Flask==0.8
-    Flask-WTF==0.6
-    Jinja2==2.6
     Markdown==2.1.1
-    PyYAML==3.10
-    WTForms==1.0.1
-    Werkzeug==0.8.3
-    distribute==0.6.19
-    docutils==0.9
--- server.py
+++ server.py
@@ -1,138 +1,64 @@
-from flask import Flask, render_template, abort, Markup, request, redirect, url_for, flash, Response, stream_with_context
-from flaskext.wtf import Form, TextField, TextAreaField, Required
+from flask import Flask, render_template, abort, Markup, request, redirect, url_for
 
-from werkzeug import secure_filename
 from markdown import markdown
-from yaml import load, safe_dump
 from jinja2 import Template
+from werkzeug.urls import url_quote_plus, url_unquote_plus
 
 import os
-from time import sleep
-from datetime import datetime
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 
 app = Flask(__name__)
-
-class PostForm(Form):
-    title = TextField(validators=[Required()])
-    body = TextAreaField(validators=[Required()])
-
-
-def get_post_metadata_generator():
-    for filename in os.listdir('posts'):
-        sleep(0.5) # add a little more lag
-        with open('posts/' + filename) as f:
-            tmp = ''
-            for line in f:
-                if not line.strip():
-                    break
-                tmp += line
-            metadata = load(tmp)
-            #TODO: add creation and modification date
-            #if not metadata.has_key('created'):
-            #    metadata['created'] = datetime.fromtimestamp(os.path.getmtime(f.name)).strftime('%Y-%m-%d %H:%M:%S')
-            metadata['filename'] = filename
-            yield metadata
-
-def get_post_metadata_list():
-    metadates = []
-    for filename in os.listdir('posts'):
-        with open('posts/' + filename) as f:
-            tmp = ''
-            for line in f:
-                if not line.strip():
-                    break
-                tmp += line
-            metadata = load(tmp)
-            #TODO: add creation and modification date
-            #if not metadata.has_key('created'):
-            #    metadata['created'] = datetime.fromtimestamp(os.path.getmtime(f.name)).strftime('%Y-%m-%d %H:%M:%S')
-            metadata['filename'] = filename
-            metadates.append(metadata)
-    return metadates
-
-def get_post_from_file(f):
-    tmp = ''
-    # read yaml stuff until blank line
-    for line in f:
-        if not line.strip():
-            break
-        tmp += line
-    post = {
-        "filename": os.path.basename(f.name),
-        "body": "".join(list(f))
-    }
-    # load yaml stuff into dict
-    post.update(load(tmp))
-    return post
-
-def save_post_to_file(f, post):
-    body = post.pop('body')
-    f.write(safe_dump(post, default_flow_style=False))
-    f.write("\n")
-    f.write(body)
 
 @app.template_filter('markdown')
 def md2html(s):
     return Markup(markdown(s))
 
-#TODO: creation and modification date should be formatted for humans, e.g. "2 minutes ago"
-#@app.template_filter()
-#def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
-#    return datetime.fromtimestamp(value).strftime(format)
-
-def stream_template(template_name, **context):
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    return t.generate(posts=get_post_metadata_generator())
-
 @app.route('/')
 def index():
-    return Response(stream_with_context(stream_template('index.html', posts=get_post_metadata_generator())))
+    posts = []
+    for filename in os.listdir('posts'):
+        title = urlsafe_b64decode(filename)
+        posts.append({'title': title, 'url': url_quote_plus(title)})
+    return render_template('index.html', posts=posts)
 
-@app.route('/p/<filename>')
-def post(filename):
+@app.route('/<url>')
+def post(url):
     try:
-        f = open('posts/' + filename)
+        title = url_unquote_plus(url)
+        f = open('posts/' + urlsafe_b64encode(title))
     except:
-        abort(404)
+        return redirect(url_for('admin_edit', url=url))
     else:
-        post = get_post_from_file(f)
+        body = f.read()
         f.close()
-        return render_template('post.html', post=post)
+        return render_template('post.html', title=title, body=body, url=url)
 
-@app.route('/admin/<filename>', methods=['GET', 'POST'])
-def admin(filename):
-    form = PostForm(csrf_enabled=False)
-    if request.method == "GET":
-        # if there is a file already, pre-fill the form
+@app.route('/new', defaults={'url': None})
+@app.route('/<url>/edit')
+def admin_edit(url):
+    title = None
+    body = ""
+    if url:
         try:
-            f = open('posts/' + filename)
+            title = url_unquote_plus(url)
+            f = open('posts/' + urlsafe_b64encode(title))
         except:
-            form.title.data = ""
-            form.body.data = ""
+            pass
         else:
-            post = get_post_from_file(f)
-            form.title.data = post['title']
-            form.body.data = post['body']
-    if request.method == "POST" and form.validate_on_submit():
-        filename = secure_filename(filename)
-        # if there is a file already, load existing data;
-        #  otherwise, create an empty dict
-        try:
-            f = open('posts/' + filename)
-            post = get_post_from_file(f)
-            post.pop('filename')
+            title = url_unquote_plus(url)
+            body = f.read()
             f.close()
-        except:
-            post = {}
-        post.update({
-            "title": form.title.data,
-            "body": form.body.data
-        })
-        with open('posts/' + filename, 'w') as f:
-            save_post_to_file(f, post)
-        return redirect(url_for('post', filename=filename))
-    return render_template('admin_posts.html', form=form, filename=filename)
+    return render_template('admin_posts.html', title=title, body=body)
+
+@app.route('/save', methods=['POST'])
+def admin_save():
+    title = request.form.get('title', None)
+    if not title:
+        return redirect(url_for('admin_edit'))
+    f = open('posts/' + urlsafe_b64encode(title), 'w')
+    f.write(request.form.get('body', ''))
+    f.close()
+    return redirect(url_for('post', url=url_quote_plus(title)))
 
 if __name__ == '__main__':
     app.run(debug=True)
